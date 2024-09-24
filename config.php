e738266f4b6a01282d7e0386b898958ac6ed6782
jessicapb <jessica.prats13@gmail.com>
2024-09-24 18:35:26 +0200

    First commit

--- config.php
+++ config.php
@@ -1,4 +1,11 @@
 <?php
-
     //constantes
     define('VIEWS',__DIR__.'/src/views');
+    define('CONTR',__DIR__.'src/controllers');
+    define('MODS',__DIR__.'src/controllers');
+    $routes=[
+        'home',
+        'add',
+        'admin'
+    ];
+?>
--- index.php
+++ index.php
@@ -1,19 +1,23 @@
 <?php
-    
     require 'config.php';
-    $url=parse_url($_SERVER['REQUEST_URI']);
-    $path=$url['path'];
-    $query=$url['query'];
-    $array_path=explode('/',$path);
-    var_dump($array_path,$query);
-    die;
-    function router($url){
-        $rutas=['add','delete','show'];
-        if(in_array($url,$rutas)){
+    function router($url,$routes){
+        if(in_array($url,$routes)){
             return $url;
         }
+        return "home";
     }
-    $contr=router($url);
-    require __DIR__.'/src/controllers/'.$contr.'.php';
-    //front controller
-  //  require VIEWS.'/index.view.php';
+    $url = $_SERVER["REQUEST_URI"];
+    $path=parse_url($url)['path'];
+    //$query=parse_url($url)['query'];
+    //var_dump($path,$query);
+    $path=explode("/",$path);
+    //var_dump($path);
+    $uri=array_filter($path,function($item){
+        if($item!=""){
+            return $item;
+        }
+    });
+    $uri=array_values($uri);
+    $contr=router($uri[0],$routes);
+    var_dump($contr);
+    //require CONTR.'add.php';
